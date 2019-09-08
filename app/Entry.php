7056f8523795b6c74ca7ecb8c547f65d7695d492
marlo <corridor.marlo@gmail.com>
2019-09-08 20:09:06 +0800

    define `Entry` relations

--- app/Entry.php
+++ app/Entry.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entry extends Model
 {
-    //
+    public function series()
+    {
+        return $this->belongsTo('App\Series');
+    }
+
+    public function account()
+    {
+        return $this->belongsTo('App\Account');
+    }
+
+    public function site()
+    {
+        return $this->belongsTo('App\Site');
+    }
 }

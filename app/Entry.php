4585008333e058a2fb34dc1f2543ce27870be468
marlo <corridor.marlo@gmail.com>
2019-09-10 09:47:44 +0800

    define Entry `is_debit` and `is_credit` accessor

--- app/Entry.php
+++ app/Entry.php
@@ -20,4 +20,15 @@ class Entry extends Model
     {
         return $this->belongsTo('App\Site');
     }
+
+    public function getIsCreditAttribute()
+    {
+        return $this->type == 'credit' ? true : false;
+    }
+
+    public function getIsDebitAttribute()
+    {
+        // reverse
+        return !$this->is_credit;
+    }
 }

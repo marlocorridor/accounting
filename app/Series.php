6c547ec31cf726464f2fd641f64f2232e8effd7b
marlo <corridor.marlo@gmail.com>
2019-09-10 13:20:53 +0800

    define Series accessors

--- app/Series.php
+++ app/Series.php
@@ -20,4 +20,50 @@ class Series extends Model
     {
         return $this->belongsTo('App\Module');
     }
+
+    public function getPoDescriptionAttribute()
+    {
+        $po = $this->purchase_order;
+        return $po ? $this->purchase_order->description : '';
+    }
+
+    public function getModuleCodeAttribute()
+    {
+        $module = $this->module;
+        return $module ? $this->module->code : '';
+    }
+
+    public function getAmountDetailsAttribute()
+    {
+        $entries = $this->entries;
+        // filter credits
+        $amount_cr = $entries->filter(function ($value, $key) {
+            return $value->is_credit;
+        })->sum('amount');
+        // filter debits
+        $amount_dr = $entries->filter(function ($value, $key) {
+            return $value->is_debit;   
+        })->sum('amount');
+
+        return [
+            'amount_cr' => $amount_cr,
+            'amount_dr' => $amount_dr,
+        ];
+    }
+
+    public function getAmountDebitAttribute()
+    {
+        return $this->amount_details['amount_dr'];
+    }
+
+    public function getAmountCreditAttribute()
+    {
+        return $this->amount_details['amount_cr'];
+    }
+
+    public function getCodeAttribute()
+    {
+        return $this->module_code . '-' . sprintf( '%08d', $this->id );
+    }
+
 }

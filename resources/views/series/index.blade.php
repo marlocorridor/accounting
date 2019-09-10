7d7dde43fe0a4633bf7ddedb67c7972c7f34d14f
marlo <corridor.marlo@gmail.com>
2019-09-10 13:21:36 +0800

    define `series.show` route

---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Series</div>
+
+                <div class="card-body">
+                    <table class="table">
+                        <tr>
+                            <th>Series No.</th>
+                            <th>PO No.</th>
+                            <th>Description</th>
+                            <th>Payee</th>
+                            <th>Amount</th>
+                        </tr>
+                    @foreach( $series as $series )
+                        <tr>
+                            <td class="text-left">
+                                <a href="{{route('series.show', $series->id)}}">
+                                    {{ $series->code }}
+                                </a>
+                            </td>
+                            <td>{{ $series->po_description }}</td>
+                            <td>{{ $series->description }}</td>
+                            <td>{{ $series->payee }}</td>
+                            <td>{{ $series->amount_debit }} | {{ $series->amount_credit }}</td>
+                        </tr>
+                    @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

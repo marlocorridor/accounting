197952b61177be5c302b4439b37e53ca0cc282af
marlo <corridor.marlo@gmail.com>
2019-09-10 10:35:51 +0800

    define `Series` relation
    entries
    purchase_order

--- app/Series.php
+++ app/Series.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Series extends Model
 {
-    //
+    public function entries()
+    {
+        return $this->hasMany('App\Entry');
+    }
+
+    public function purchase_order()
+    {
+        return $this->belongsTo('App\PurchaseOrder');
+    }
 }

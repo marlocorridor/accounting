16d8636d7874216232fccf5d00f421bff936cb57
marlo <corridor.marlo@gmail.com>
2019-09-11 11:12:21 +0800

    create `TextInput` vue component

---
+++ resources/js/components/debit-credit/TextInputComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="input-group mb-3">
+        <input v-if="isCredit" type="text" class="form-control disabled" disabled>
+        <input type="number" step="0.01" class="form-control" placeholder="Amount" v-model.lazy="value">
+        <input v-if="isDebit" type="text" class="form-control disabled" disabled>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: 'debit-credit-text-input',
+                activeClass: 'btn-primary',
+                input_name: '',
+                value: 0.00,
+            }
+        },
+        props: {
+            entry_type: {
+                type: String,
+                default: 'debit',
+            },
+        },
+        mounted() {
+            console.log('Text Input Component mounted.')
+        },
+        watch: {
+            value: function (newVal, oldVal) {
+                // if false
+                if (newVal) {
+                    console.log(newVal, 'newVal')
+                }
+                this.$emit('update_value', newVal)
+            }
+        },
+        computed: {
+            isDebit: function () {
+                return this.entry_type == 'debit'
+            },
+            isCredit: function () {
+                return this.entry_type == 'credit'
+            }
+        }
+    }
+</script>

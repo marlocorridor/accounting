cf1bb6879f5a2c0437bc4265e8773b4462a2cea1
marlo <corridor.marlo@gmail.com>
2019-09-16 17:03:41 +0800

    move table headers to `entries` component

--- resources/js/components/EntriesComponent.vue
+++ resources/js/components/EntriesComponent.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="entries-container">
+        <div class="h1">
+            <span>{{ series.payee }}</span> | <small>{{ series.description }}</small>
+        </div>
+        <div class="d-flex align-items-center row text-center font-weight-bold">
+            <div class="col-sm-1">Type</div>
+            <div class="col-sm-3">Account</div>
+            <div class="col-sm-3">Description</div>
+            <div class="col-sm-2">Site</div>
+            <div class="col-sm-3">
+                <div>Amount</div>
+                <div class="row">
+                    <label class="col-sm-6">Debit</label>
+                    <label class="col-sm-6">Credit</label>
+                </div>
+            </div>
+        </div>
         <entry-component
             v-for="(entry, index) in entries"
             v-bind:entry="entry"
--- resources/views/series/entries.blade.php
+++ resources/views/series/entries.blade.php
@@ -7,19 +7,6 @@
     <div class="row justify-content-center">
         <div class="col-md-12 mt-2">
             <div id="app">
-                <div class="d-flex align-items-center row text-center font-weight-bold">
-                    <div class="col-md-1">Type</div>
-                    <div class="col-md-3">Account</div>
-                    <div class="col-md-3">Description</div>
-                    <div class="col-md-2">Site</div>
-                    <div class="col-md-3">
-                        <div>Amount</div>
-                        <div class="row">
-                            <label class="col-md-6">Debit</label>
-                            <label class="col-md-6">Credit</label>
-                        </div>
-                    </div>
-                </div>
                 <entries-component></entries-component>
             </div>
         </div>
